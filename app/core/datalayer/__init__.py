bebeb9f6e0e06ebf1ff2965503b729bffbd60030
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-16 14:28:36 +0700

    feat: add BaseDataManager for data handling

--- app/core/datalayer/__init__.py
+++ app/core/datalayer/__init__.py
@@ -1,8 +1,8 @@
-from app.core.datalayer.base_repo import BaseRepository
-from app.core.datalayer.base_service import BaseService
-from app.core.datalayer.datamanager import (
+from app.core.datalayer.base_data import (
     BaseDataManager,
 )
+from app.core.datalayer.base_repo import BaseRepository
+from app.core.datalayer.base_service import BaseService
 
 __all__ = [
     "BaseDataManager",
