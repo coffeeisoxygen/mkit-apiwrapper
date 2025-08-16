d81c507cde140f7b9165af15e9ea9a19eb42192a
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-16 12:07:00 +0700

    refactor: update router registration with prefix and tags
    Refactor the register_routers function to include the prefix and tags for the debug router. This improves the organization and clarity of the API routes.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,8 @@ from app.api.v1.debug.debug import router as debug_router
 
 
 def register_routers(app):  # noqa: ANN001, D103
-    app.include_router(debug_router)
+    app.include_router(
+        debug_router,
+        prefix="/api/v1",
+        tags=["Debug"],
+    )
--- app/api/v1/debug/debug.py
+++ app/api/v1/debug/debug.py
@@ -7,16 +7,16 @@ from app.deps import DepAppSettings, DepMemberService, DepModuleService
 router = APIRouter()
 
 
-@router.get("/debug/members", tags=["Debug"])
+@router.get("/debug/members")
 def debug_members(member_service: DepMemberService):
     return {"members": [m.model_dump() for m in member_service.get_all()]}
 
 
-@router.get("/debug/modules", tags=["Debug"])
+@router.get("/debug/modules")
 def debug_modules(module_service: DepModuleService):
     return {"modules": [m.model_dump() for m in module_service.get_all()]}
 
 
-@router.get("/debug/settings", tags=["Debug"])
+@router.get("/debug/settings")
 def debug_settings(app_settings: DepAppSettings):
     return app_settings
