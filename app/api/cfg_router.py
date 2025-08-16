074aa884f0bb4cbe93a61ae3ea24d7d4a0b956cd
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-16 11:13:52 +0700

    feat: make register_routers asynchronous

--- app/api/cfg_router.py
+++ app/api/cfg_router.py
@@ -3,5 +3,5 @@
 from app.api.v1.debug.debug import router as debug_router
 
 
-def register_routers(app):
+async def register_routers(app):  # noqa: ANN001, D103, RUF029
     app.include_router(debug_router)
